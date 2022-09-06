6dec3d6b259d7ef780ab9f8e0d640fe3bfb6bf5b
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 23:12:56 -0500

    Update 1_CBSAs_by_State.py

--- pages/1_CBSAs_by_State.py
+++ pages/1_CBSAs_by_State.py
@@ -3,6 +3,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 # import 
 components.html("""
-                   <iframe title="RJ_CBSA - Overview" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true"></iframe>
+                   <iframe title="RJ_CBSA - Overview" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true"></iframe>
                    """)
 
