b469df6a58bfc5f3c942ceffb04ea005827ed729
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-06 07:14:23 -0500

    Update 3_Map.py

--- pages/3_Map.py
+++ pages/3_Map.py
@@ -1,1 +1,21 @@
 
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 07:11:00 2022
+
+@author: Zach
+"""
+
+
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+
+df = pd.read_csv('help.csv')
+
+inputForm = st.form("State Input")
+inputState = inputForm.text_input(label='Enter State',value='OH')
+inputState = inputState.upper()
+submit_button = inputForm.form_submit_button("All Details")
+
+st.map(df)
