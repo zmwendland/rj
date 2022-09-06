994509258d9c79bbe773a437780e118c5da06b35
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 23:19:57 -0500

    Update 1_CBSAs_by_State.py

--- pages/1_CBSAs_by_State.py
+++ pages/1_CBSAs_by_State.py
@@ -9,7 +9,8 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 # import 
-components.html("""
-                   <a href='https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9&pageName=ReportSection'></a>
+f = components.html("""
+                   <iframe title="RJ_CBSA - Overview" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true" width = "100%"></iframe>
                    """)
 
+st.write(f)
