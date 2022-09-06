fb657ecfa818de0bf70f4600e5976292935cd2db
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 23:29:22 -0500

    Update 1_CBSAs_by_State.py

--- pages/1_CBSAs_by_State.py
+++ pages/1_CBSAs_by_State.py
@@ -10,8 +10,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 # import 
 
-height = st.sidebar.slider("Height", 200, 1500, 300, 100)
-width = st.sidebar.slider("Width", 200, 1500, 600, 100)
+height = st.sidebar.slider("Height", 200, 2000, 300, 100)
+width = st.sidebar.slider("Width", 200, 2000, 600, 100)
 
 components.html("""
                    <iframe title="RJ_CBSA - Overview" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true"></iframe>
