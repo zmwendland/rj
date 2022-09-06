12f5dc13a00649f3b8842b925ca70fae46efee20
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-06 07:30:39 -0500

    Update 3_Map.py

--- pages/3_Map.py
+++ pages/3_Map.py
@@ -20,6 +20,8 @@ submit_button = inputForm.form_submit_button("All Details")
 if submit_button:
     df1 = df.loc[df['state_id']==inputState,'lat']
     df2 = df.loc[df['state_id']==inputState,'lon']
-    fdf = pd.concat([df1,df2],axis=1)
+    df3 = df.loc[df['state_id']==inputState,'cbsa_name']
+    fdf = pd.concat([df1,df2,df3],axis=1)
+    fdf = fdf.drop_duplicates()
     st.map(fdf)
 
