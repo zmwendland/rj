0483becb811a41fa04adaf39b871acef83a3ae58
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 20:37:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if city_button:
      df3 = file.loc[file['state_id'] == inputState,'city']
      df4 = file.loc[file['state_id'] == inputState,'zip']
      fdf = pd.concat([df3,df4],axis=1)
-     fdf['zip_count'] = fdf.groupby('city')['zip'].transform('count')
+     fdf['cnt'] = fdf.groupby('city')['zip'].transform('count')
      fdf = fdf.drop_duplicates(subset='city')
      fdf = fdf.drop(['zip'],axis=1)
      pd.set_option('display.max_columns', None)
