746c42f9caf8d3fbe0fb09e29e1013df56375939
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 23:07:52 -0500

    Update 1_CBSAs_by_State.py

--- pages/1_CBSAs_by_State.py
+++ pages/1_CBSAs_by_State.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
+# import 
+components.html("""
+                   <iframe title="RJ_CBSA - Overview" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true"></iframe>
+                   """)
 
-df = pd.read_csv('cbsa.csv')
-
-title = st.text_input(label='Enter State',value='OH')
-st.write(df.loc[df['state_id'] == title,'cbsa_name'])
