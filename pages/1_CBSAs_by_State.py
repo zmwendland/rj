1393f41ff9854039ec74423f794034d00114a890
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 23:17:46 -0500

    Update 1_CBSAs_by_State.py

--- pages/1_CBSAs_by_State.py
+++ pages/1_CBSAs_by_State.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 21:55:22 2022
+
+@author: Zach
+"""
+
 import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 # import 
 components.html("""
-                   <iframe title="RJ_CBSA - Overview" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9" frameborder="0" allowFullScreen="true"></iframe>
+                   <a href='https://app.powerbi.com/view?r=eyJrIjoiMTFkMDNmZWQtMTQzNi00N2RmLTk0MTctM2IwZGNkMTYzZmExIiwidCI6Ijc5NmMxMTY3LTRjNTEtNDkxZC05MDhjLWZiNmJjMThiMDIyZiJ9&pageName=ReportSection'></a>
                    """)
 
