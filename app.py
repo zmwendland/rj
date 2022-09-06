fe843fe7cceb8c2d4161dff9018b66ea5858f703
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-05 19:52:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,15 +45,15 @@ if submit_button:
     df3 = file.loc[file['state_id'] == inputState,'city']
     df4 = file.loc[file['state_id'] == inputState,'zip']
     fdf = pd.concat([df,df2,df3,df4],axis=1)
-    st.write(fdf)
+    st.table(fdf)
 if cbsa_button:
     df = sta_msa.loc[sta_msa['state_id']==inputState,'cbsa_name']
     df = df.drop_duplicates()
-    st.write(df)
+    st.table(df)
 if cnty_button:
     df2 = file.loc[file['state_id'] == inputState,'county_name']
     df2 = df2.drop_duplicates()
-    st.write(df2)
+    st.table(df2)
 if city_button:
      df3 = file.loc[file['state_id'] == inputState,'city']
      df4 = file.loc[file['state_id'] == inputState,'zip']
@@ -62,7 +62,7 @@ if city_button:
      fdf = fdf.drop_duplicates(subset='city')
      fdf = fdf.drop(['zip'],axis=1)
      # fdf = fdf.drop_duplicates()
-     st.write(fdf)
+     st.table(fdf)
  
 
     
